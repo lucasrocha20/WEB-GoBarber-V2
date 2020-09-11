f53423b48b34bf6adf918fe39f7bf6e36cc12f2d
Lucas Rocha <lucas_rocha.14@outlook.com>
2020-09-10 22:34:52 -0300

    finish toast styles

--- src/components/ToastContainer/index.tsx
+++ src/components/ToastContainer/index.tsx
@@ -6,7 +6,32 @@ import { Container, Toast } from './styles';
 const ToastContainer: React.FC = () => {
   return (
     <Container>
-      <Toast>
+      <Toast hasDescription>
+        <FiAlertCircle size={20} />
+
+        <div>
+          <strong>Aconteceu um erro</strong>
+          <p>Não foi possível fazer login na aplicação</p>
+        </div>
+
+        <button type="button">
+          <FiXCircle size={18} />
+        </button>
+      </Toast>
+
+      <Toast type="success" hasDescription={false}>
+        <FiAlertCircle size={20} />
+
+        <div>
+          <strong>Aconteceu um erro</strong>
+        </div>
+
+        <button type="button">
+          <FiXCircle size={18} />
+        </button>
+      </Toast>
+
+      <Toast type="error" hasDescription>
         <FiAlertCircle size={20} />
 
         <div>
--- src/components/ToastContainer/styles.ts
+++ src/components/ToastContainer/styles.ts
@@ -1,4 +1,26 @@
-import styled from 'styled-components';
+import styled, { css } from 'styled-components';
+
+interface ToastProps {
+  type?: 'success' | 'error' | 'info';
+  hasDescription: boolean;
+}
+
+const toastTypeVariations = {
+  info: css`
+    background: #ebf8ff;
+    color: #3172b7;
+  `,
+
+  success: css`
+  background: #e6fffa;
+  color: #2e656a;
+  `,
+
+  error: css`
+  background: #fddede;
+  color: #c53030;
+  `,
+}
 
 export const Container = styled.div`
   position: absolute;
@@ -8,7 +30,7 @@ export const Container = styled.div`
   overflow: hidden;
 `;
 
-export const Toast = styled.div`
+export const Toast = styled.div<ToastProps>`
   width: 360px;
 
   position: relative;
@@ -18,8 +40,11 @@ export const Toast = styled.div`
 
   display: flex;
 
-  background: #ebf8ff;
-  color: #3172b7;
+  & + div {
+    margin-top: 8px;
+  }
+
+  ${(props) => toastTypeVariations[props.type || 'info']}
 
   > svg {
     margin: 4px 12px 0 0;
@@ -45,4 +70,14 @@ export const Toast = styled.div`
     background: transparent;
     color: inherit;
   }
+
+  ${(props) =>
+    !props.hasDescription &&
+    css`
+      align-items: center;
+
+      svg {
+        margin-top: 0;
+      }
+    `}
 `;
