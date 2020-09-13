d73d8ecf870addc53fbf3baeb465f368da513c8e
Lucas Rocha <lucas_rocha.14@outlook.com>
2020-09-13 10:40:03 -0300

    creating routers with react-router-dom

--- src/pages/SignIn/styles.ts
+++ src/pages/SignIn/styles.ts
@@ -1,4 +1,4 @@
-import styled from 'styled-components';
+import styled, { keyframes } from 'styled-components';
 import { shade } from 'polished';
 
 import signInBackgroundImg from '../../assets/sign-in-background.png';
@@ -11,6 +11,23 @@ export const Container = styled.div`
 `;
 
 export const Content = styled.div`
+  width: 100%;
+  max-width: 700px;
+  margin: 50px 0;
+`;
+
+const appearFromLeft = keyframes`
+  from {
+    opacity: 0;
+    transform: translateX(-50px);
+  }
+  to {
+    opacity: 1;
+    transform: translateX(0);
+  }
+`;
+
+export const AnimationContainer = styled.div`
   display: flex;
   flex-direction: column;
 
@@ -18,9 +35,7 @@ export const Content = styled.div`
   align-items: center;
   justify-content: center;
 
-  width: 100%;
-  max-width: 700px;
-  margin: 50px 0;
+  animation: ${appearFromLeft} 1s;
 
   form {
     margin: 40px 0;
